103eba4edd61786c626cd52c80fef0748aecab61
LevkoBe <lbeniakh@kse.org.ua>
2025-07-10 12:52:22 +0300

    feat: view controls

---
+++ src/ui/ViewControls.ts
@@ -0,0 +1,36 @@
+import { AppState } from "../state/AppState.ts";
+import { DOMManager } from "./DOMManager.ts";
+
+export class ViewControls {
+  private appState: AppState;
+  private domManager: DOMManager;
+
+  constructor(appState: AppState, domManager: DOMManager) {
+    this.appState = appState;
+    this.domManager = domManager;
+  }
+
+  public zoomIn = (): void => {
+    this.appState.zoom *= 1.2;
+    this.domManager.setCanvasTransform(
+      this.appState.zoom,
+      this.appState.pan.x,
+      this.appState.pan.y
+    );
+  };
+
+  public zoomOut = (): void => {
+    this.appState.zoom /= 1.2;
+    this.domManager.setCanvasTransform(
+      this.appState.zoom,
+      this.appState.pan.x,
+      this.appState.pan.y
+    );
+  };
+
+  public resetView = (): void => {
+    this.appState.zoom = 1;
+    this.appState.pan = { x: 0, y: 0 };
+    this.domManager.setCanvasTransform(1, 0, 0);
+  };
+}
